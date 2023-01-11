3e74738a0875d313d0691df95b661de92c096d78
Oladejo Toyyib <oladejotoyyibolalekan@gmail.com>
2023-01-11 11:05:57 +0100

    first commit

--- script.js
+++ script.js
@@ -14,3 +14,48 @@ const updatedDisplay = () => {
 };
 
 updatedDisplay();
+
+// Handle Key Press
+
+const keys = document.querySelector('.key');
+keys.addEventListener('click', (event) => {
+    const { target } = event:
+    if (!target.matches('button')) {
+        return;
+    }
+
+    if(target.classList.contains('operator')) {
+        handleOperator(target.value)
+        updatedDisplay()
+        return;
+    }
+
+    if (target.classList.contains('decimal')) {
+        inputDecimal(target.value);
+        updatedDisplay()
+        return;
+    }
+    if (target.classList.contains('decimal')) {
+        resetCalculator()
+        updatedDisplay()
+        return;
+    }
+        inputDigit(target.value);
+        updatedDisplay();
+});
+
+//Input Digit
+
+const inputDigit = (digit) {
+    const { displayValue, waitingForSeconsOperand } = calculator;
+
+    if(waitingForSeconsOperand === true) {
+        calculator.displayValue = digit;
+        calculator.waitingForSecondOperang = false;
+    } else {
+        calculator.displayValue = 
+        displayValue === "0" ? digit: displayValue  + digit
+    }
+    }
+
+
